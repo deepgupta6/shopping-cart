2a88f88245942637c0384fdabd0ba034e8d5500b
Deep Gupta <deep2004gupta@gmail.com>
2025-08-01 09:12:05 +0530

    update baseURL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8080' });
+const API = axios.create({ baseURL: 'https://deep-shopping-cart.vercel.app/' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('token')) {
